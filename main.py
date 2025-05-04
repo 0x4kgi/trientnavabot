892b2961977880e8d0a36ed8175fcfe64b06f672
0x4kgi <17591127+0x4kgi@users.noreply.github.com>
2025-05-04 16:44:13 +0800

    make the bot prefix configurable in .env

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import os
 import discord
 from discord.ext import commands
 
+load_dotenv()
+
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(
-    command_prefix=';',
+    command_prefix=os.getenv('BOT_PREFIX'),
     intents=intents,
 )
 
@@ -37,6 +39,6 @@ async def reload(ctx: commands.Context, cog: str):
 
 
 if __name__ == '__main__':
-    load_dotenv()
+    
 
     bot.run(os.getenv("TOKEN"))
